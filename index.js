994528e90d21b0ac5c926b44316078759ca97da1
nichllaurado <n1llaurado@gmail.com>
2025-08-28 15:49:52 -0400

    more buttons

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+let currentChips = 1000;
+let currentBetAmount = 0;
 
 
 function setBetAmt(betAmount) {
     const betDispDiv = document.getElementById("betamt-div");
     betDispDiv.innerHTML = "";
-
+    console.log("selected bet amount " + betAmount);
 
     //create element
     const betAmountText = document.createElement("div");
-    //betAmountText.id = "cycle-" + i;
+    betAmountText.id = "cycle-";
     betAmountText.textContent = betAmount;
+    currentBetAmount = betAmount;
+    betDispDiv.appendChild(betAmountText);
+}
+
+function deal() {
+    currentChips -= currentBetAmount;
+    const chipsDiv = document.getElementById("chipsDiv");
+    chipsDiv.textContent = "Current chips: " + currentChips;
 }
 
